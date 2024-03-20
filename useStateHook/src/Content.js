cb765f3bbde420e5cc7ea00903bb96b33fc20505
DevOluwatunmise <tunmisedare@gmail.com>
2024-03-20 12:56:47 +0100

    update

--- useStateHook/src/Content.js
+++ useStateHook/src/Content.js
@@ -1,10 +1,10 @@
-import { FaTrash } from "react-icons/fa";
+import { IoMdTrash } from "react-icons/io";
 import { useState } from "react";
 import "./content.css";
 
 const Content = () => {
   // const [name, setName] = useState('Basirah')     //will set initial state to be Basirah, this is example of useState, set name will be changing other name
-  const [item, setItem] = useState([
+  const [items, setItems] = useState([
     {
       id: 1,
       checked: false,
@@ -27,35 +27,54 @@ const Content = () => {
     },
   ]);
 
-  // const handleNameChange = () => {
-  //     const names = ["Oluwatunmise","fulaani", "ooye", "Eef"];
-  //     const int = Math.floor(Math.random() * 3);
-  //     setName(names[int]);
-  // };
+  
 
-  // const handleClick2 = (name) => {
-  //   console.log(`${name} was clicked`)
-  // }
+  const handleCheck = (id) => {
+    // console.log(`key: ${id}`)
+    const listItems = items.map((item) => 
+    item.id === id ? {...item, checked: !item.checked } : item  // read more abpout this line
+    
+    );
+    setItems(listItems)   // it update it from false to true(from initial state{setItems} to useState {listItem})
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems))
+  };
+
+  const handleDelete = (id) => {    // we are using tinary operator to pass the condition and we filter through
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems)   // we now call it out like this
+  }
 
   return (
     <main className="student">
-      {/* <h1>{name}</h1>
-      <button onClick={handleNameChange}>Change Name</button>
-       <button onClick={handleClick}>Click me</button>   {/* then you will write it like this under return so that it will show */}
-      {/* <button onDoubleClick={() => handleClick2('Hayzed')}>Click me</button>   */}{" "}
+       
 
-      <ul>
-        {item.map((item) => {
+      {items.length ? (
+        <ul>
+        {items.map((item) => {
           return (
             <li className="item" key={item.id}>
-              <input type="checkbox" checked={item.checked} />
+              <input type="checkbox" 
+              onClick={() => handleCheck(item.id)}  // this code make the box cliceable and show key and number in the console(anonymus function)
+              checked={item.checked} />
+              
 
-              <label>{item.item}</label>
-              <FaTrash/>
+              <label
+              style={(item.checked) ? {textDecoration: "line-through"} : null}
+              onDoubleClick={() => handleDelete(item.id)}
+              >{item.item}</label>
+              <IoMdTrash
+              onClick={() => handleDelete(item.id)}   // to make it delete, we put the anonymus funtion inside the trash icon 
+              role="button"
+              tabIndex="0"
+              />
             </li>
           );
         })}
       </ul>
+      ): (
+        <p style={{margingTop: "2rem" }}>Your list is empty</p>
+      )
+      }
     </main>
   );
 };
