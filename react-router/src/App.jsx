29018d58223ace7bf5079fb604aadb0975cfed68
DevOluwatunmise <tunmisedare@gmail.com>
2024-04-04 14:15:33 +0100

    update

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,5 +1,5 @@
 import {Route, Routes} from "react-router-dom"
-import React from 'react'
+import { useState } from 'react'
 import Header from './Header'
 import Nav from './Nav'
 import Home from './Home'
@@ -10,21 +10,58 @@ import Missing from './Missing'
 import Footer from './Footer'
 
 const App = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My first post",
+      dateTime: "April 04, 2024 11:01 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quaerat deleniti voluptatibus placeat illo tenetur enim repudiandae distinctio,nobis perferendis accusamus, facere consectetur quos cumque labore mollitia sequi ea adipisci?"
+    },
+    {
+      id: 2,
+      title: "My second post",
+      dateTime: "April 04, 2024 11:01 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quaerat deleniti voluptatibus placeat illo tenetur enim repudiandae distinctio,nobis perferendis accusamus, facere consectetur quos cumque labore mollitia sequi ea adipisci?"
+    },
+    {
+      id: 3,
+      title: "My third post",
+      dateTime: "April 04, 2024 11:01 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quaerat deleniti voluptatibus placeat illo tenetur enim repudiandae distinctio,nobis perferendis accusamus, facere consectetur quos cumque labore mollitia sequi ea adipisci?"
+    },
+    {
+      id: 4,
+      title: "My forth post",
+      dateTime: "April 04, 2024 11:01 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quaerat deleniti voluptatibus placeat illo tenetur enim repudiandae distinctio,nobis perferendis accusamus, facere consectetur quos cumque labore mollitia sequi ea adipisci?"
+    },
+    {
+      id: 5,
+      title: "My fifth post",
+      dateTime: "April 04, 2024 11:01 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quaerat deleniti voluptatibus placeat illo tenetur enim repudiandae distinctio,nobis perferendis accusamus, facere consectetur quos cumque labore mollitia sequi ea adipisci?"
+    },
+  ])
+  const [search, setSearch] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  const handleDelete = () => {
 
-
+  }
   return (
     <div className='App'>
-      <Header/>
-      <Nav/>
+      <Header title= "DLT Blogs"/>
+      <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home posts={posts}/>}/>      //** forward slash(/) means or specify home page **//
+        <Route path="/post" element={<NewPost/>} />
+        <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete}/>}/>
         <Route path="/about" element={<About/>} />
-        <Route path="/post-page" element={<PostPage/>} />
+        <Route path="*" element={<Missing/>} />   //** the outsterisk will show you that the page can not be found **/
       </Routes>
       <Footer/>
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
